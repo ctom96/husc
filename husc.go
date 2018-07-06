b560f23ec28d47a5822fcab1143aeeae4d3485fd
ctom96 <ctom96@vt.edu>
2018-07-05 20:13:52 -0400

    Using the flags import for better command line arguement processing

--- husc.go
+++ husc.go
@@ -2,46 +2,52 @@
 //
 // HUSC defines a data format, and the husc tool is used to
 // convert from HUSC to JSON or JSON to HUSC.
-
 package main
 
 import (
+	"flag"
 	"fmt"
-	"husc/huscutil"
-	"os"
 	"strings"
 )
 
-func printUsage() {
-	fmt.Println("ERROR: Incorrect Usage!")
-	fmt.Println("Usage: husc [options...] [files...]")
-}
-
 func main() {
-	args := os.Args[1:]
 
-	if len(args) < 2 {
-		printUsage()
-		return
+	// Process command-line arguements with "flag" import
+	var toJSON bool
+	flag.BoolVar(&toJSON, "json", false, "Convert specified .husc files to JSON files")
+	flag.BoolVar(&toJSON, "j", false, "Convert specified .husc files to JSON files (shorthand)")
+
+	var toHUSC bool
+	flag.BoolVar(&toHUSC, "husc", false, "Convert specified .husc files to HUSC files")
+	flag.BoolVar(&toHUSC, "h", false, "Convert specified .husc files to HUSC files (shorthand)")
+
+	flag.Parse()
+
+	files := flag.Args()
+
+	if toJSON {
+		for _, file := range files {
+			if !strings.HasSuffix(file, ".husc") {
+				fmt.Println("Not every file given is of type .husc.\nExiting...")
+				return
+			}
+		}
+		fmt.Println("This is the point where we would convert your files to .json...")
+
+	} else if toHUSC {
+		for _, file := range files {
+			if !strings.HasSuffix(file, ".json") {
+				fmt.Println("Not every file given is of type .json.\nExiting...")
+				return
+			}
+		}
+		fmt.Println("This is the point where we would convert your files to .husc...")
+	} else {
+		fmt.Println("No -j or -h flag specified.")
+		flag.Usage()
 	}
 
-	// TODO Add verbose mode
-	//verbose := false
-	var files []string
-
-	// Process args
-	switch strings.ToLower(args[0]) {
-	case "-j", "-json":
-		// Convert list of files into json formatted files
-		files := append(files, args[1:]...)
-
-		fmt.Println(files)
-		fmt.Println(huscutil.ConvertToJSON(files))
-
-	case "-h", "-husc":
-		// Convert json to husc formatted files of the same name
-		files := append(files, args[1:]...)
-
-		fmt.Println(files)
-	}
 }
+
+// TODO: Write the library to convert from husc to json
+// TODO: Write the library to convert from json to husc
